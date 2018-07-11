372cb426ce9872946c661160c50179c1147bc5e5
EricrPark <League@AdministorsiMac.attlocal.net>
2018-07-11 11:57:26 -0700

    Finished 00 working on 01

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Test;
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+	String[] words = {"lame", "lamer", "lamest", "lamerest"};
 	@Test
 	void testLinearSearch() {
+		_00_LinearSearch lin = new _00_LinearSearch();
 		//1. use the assertEquals method to test your linear search method.
+		assertEquals(3, lin.linearSearch(words, words[3]));
+		assertEquals(1, lin.linearSearch(words, words[1]));
+		assertEquals(4, lin.linearSearch(words, words[4]));
 	}
 
 	@Test
